5a4c48f45cd4db6af69ffa2479e9ee697e3e59b3
Brandon Andersen <brandonjandersen@gmail.com>
2020-03-25 12:05:44 -0400

    add css grid example app

---
+++ cssgrid/app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 25 11:26:31 2020
+
+@author: brandonandersen
+"""
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash()
+
+app.layout = html.Div(className='wrapper',
+                      children=[
+                              html.Div(className='header', children=dcc.Markdown('Page Title')),
+                              html.Div(className='menu', children=dcc.Markdown('Menu')),
+                              html.Div(className='top-content', children=dcc.Markdown('Top Content')),
+                              html.Div(className='bottom-content', children=dcc.Markdown('Bottom Content'))
+                              ])
+
+if __name__ == '__main__':
+    app.run_server()
